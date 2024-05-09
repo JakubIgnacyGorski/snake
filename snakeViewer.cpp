de86561c1cb45ac03f1f85b2c8775ce4a2f5447d
Jakub Górski <278291@student.pwr.edu.pl>
2024-05-09 16:11:11 +0200

    Dodanie biblioteki SFML

---
+++ snakeViewer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jakub on 09.05.24.
+//
+
+#include "snakeViewer.h"
---
+++ snakeViewer.h
@@ -0,0 +1,14 @@
+//
+// Created by jakub on 09.05.24.
+//
+
+#ifndef SNAKE_SNAKEVIEWER_H
+#define SNAKE_SNAKEVIEWER_H
+
+
+class snakeViewer {
+
+};
+
+
+#endif //SNAKE_SNAKEVIEWER_H
