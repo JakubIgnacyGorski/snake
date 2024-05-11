96f0cb30e567a7b1840516f781e167840f379ba5
Jakub Górski <278291@student.pwr.edu.pl>
2024-05-11 18:55:02 +0200

    Dodanie menu

---
+++ snakeMenu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jakub on 11.05.24.
+//
+
+#include "snakeMenu.h"
---
+++ snakeMenu.h
@@ -0,0 +1,14 @@
+//
+// Created by jakub on 11.05.24.
+//
+
+#ifndef SNAKE_SNAKEMENU_H
+#define SNAKE_SNAKEMENU_H
+
+
+class snakeMenu {
+
+};
+
+
+#endif //SNAKE_SNAKEMENU_H
