2be8952bb7155203f26e0d6b70106c48ede871aa
Jakub Górski <278291@student.pwr.edu.pl>
2024-05-26 22:36:52 +0200

    Dodanie stalych odpowiadających za kolory

---
+++ ColorsConsts.h
@@ -0,0 +1,19 @@
+//
+// Created by jakub on 26.05.24.
+//
+
+#ifndef SNAKE_COLORSCONSTS_H
+#define SNAKE_COLORSCONSTS_H
+
+#include <SFML/Graphics/Color.hpp>
+
+const sf::Color backgroundColor(98,225,177);
+const sf::Color scoreBackgroundColor{47,201,143};
+const sf::Color buttonColor{218,225,98};
+const sf::Color snakeBodyColor{69,216,8};
+const sf::Color snakeHeadColor{snakeBodyColor};
+//const sf::Color snakeBackColor{snakeBodyColor};
+const sf::Color snakeFoodColor{225,98,31};
+const sf::Color textColor{24,24,24};
+
+#endif //SNAKE_COLORSCONSTS_H
