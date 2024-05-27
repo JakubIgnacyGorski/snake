13fe6e0bd51214add48de8fe358f45ef2b85453f
Jakub Górski <278291@student.pwr.edu.pl>
2024-05-28 00:41:49 +0200

    Dodanie grafiki

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -9,23 +9,88 @@
 
 TextureManager::TextureManager() {
 
-    if (!background.loadFromFile("textures/background.png")) {
+    if (!background.loadFromFile("textures/background/runningBackground.png")) {
         std::cerr << strerror(errno) << std::endl;
         abort();
     }
     background.setRepeated(true); //źródło: https://www.sfml-dev.org/documentation/2.6.1/classsf_1_1Texture.php#aaa87d1eff053b9d4d34a24c784a28658
+    if (!snakeHead[movDir{UP}].loadFromFile("textures/snake/head/head-up.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeHead[movDir{RIGHT}].loadFromFile("textures/snake/head/head-right.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeHead[movDir{DOWN}].loadFromFile("textures/snake/head/head-down.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeHead[movDir{LEFT}].loadFromFile("textures/snake/head/head-left.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeBody[0].loadFromFile("textures/snake/body/body-horizontal.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeBody[1].loadFromFile("textures/snake/body/body-vertical.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeBack[movDir{UP}].loadFromFile("textures/snake/tail/tail-up.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeBack[movDir{RIGHT}].loadFromFile("textures/snake/tail/tail-right.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeBack[movDir{DOWN}].loadFromFile("textures/snake/tail/tail-down.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeBack[movDir{LEFT}].loadFromFile("textures/snake/tail/tail-left.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeTurn[turnDir{DOWNLEFT}].loadFromFile("textures/snake/turn/turn-down-left.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeTurn[turnDir{DOWNRIGHT}].loadFromFile("textures/snake/turn/turn-down-right.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeTurn[turnDir{UPLEFT}].loadFromFile("textures/snake/turn/turn-up-left.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
+    if (!snakeTurn[turnDir{UPRIGHT}].loadFromFile("textures/snake/turn/turn-up-right.png")) {
+        std::cerr << strerror(errno) << std::endl;
+        abort();
+    }
 }
 
 const sf::Texture * TextureManager::getBackgroundTexture() const {
     return &background;
 }
 
-const sf::Texture *TextureManager::getSnakeHead(const int dir) const {
-    if (dir>=4 || dir<0) return nullptr;
+const sf::Texture *TextureManager::getSnakeHead(const movDir dir) const {
     return &snakeHead[dir];
 }
 
-const sf::Texture *TextureManager::getSnakeBody(const int dir) const {
-    if (dir>=4 || dir<0) return nullptr;
-    return &snakeBody[dir];
+const sf::Texture *TextureManager::getSnakeBody(const movDir dir) const {
+    if (dir == UP || dir == DOWN) return &snakeBody[0];
+    return &snakeBody[1];
 }
+
+const sf::Texture *TextureManager::getSnakeBack(const movDir dir) const {
+    return &snakeBack[dir];
+}
+
+const sf::Texture *TextureManager::getSnakeTurn(const turnDir dir) const {
+    return &snakeTurn[dir];
+}
+
+
--- TextureManager.h
+++ TextureManager.h
@@ -6,15 +6,22 @@
 #define SNAKE_TEXTUREMANAGER_H
 #include <SFML/Graphics/Texture.hpp>
 
+enum movDir { UP, RIGHT, DOWN, LEFT};
+enum turnDir { DOWNLEFT, DOWNRIGHT, UPLEFT, UPRIGHT};
+
 class TextureManager {
     sf::Texture snakeHead[4];
-    sf::Texture snakeBody[4];
+    sf::Texture snakeBack[4];
+    sf::Texture snakeTurn[4];
+    sf::Texture snakeBody[2];
     sf::Texture background;
 public:
     TextureManager();
     const sf::Texture * getBackgroundTexture() const;
-    const sf::Texture * getSnakeHead(int dir) const;
-    const sf::Texture * getSnakeBody(int dir) const;
+    const sf::Texture * getSnakeHead(movDir dir) const;
+    const sf::Texture * getSnakeBody(movDir dir) const;
+    const sf::Texture * getSnakeBack(movDir dir) const;
+    const sf::Texture * getSnakeTurn(turnDir dir) const;
 };
 
 
